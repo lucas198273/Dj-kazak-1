db1fdcb25961b42435e47fd52a6f726455067475
Lucas Dias <lucasinga2000@gmail,com>
2025-05-26 17:37:28 -0300

    0

--- src/components/LazySoundCloudPlayer/LazySoundCloudPlayer.tsx
+++ src/components/LazySoundCloudPlayer/LazySoundCloudPlayer.tsx
@@ -9,10 +9,6 @@ const LazySoundCloudPlayer: React.FC<LazySoundCloudPlayerProps> = ({ trackUrl })
 
   return (
     <div style={{ minHeight: '300px' }}>
-      {/*
-        - Enquanto o usuário não clicar, mostramos somente um botão.
-        - Assim que clicar, renderizamos o <iframe> de uma vez só, sem depender de IntersectionObserver.
-      */}
       {!showPlayer ? (
         <button
           onClick={() => setShowPlayer(true)}
@@ -34,12 +30,19 @@ const LazySoundCloudPlayer: React.FC<LazySoundCloudPlayerProps> = ({ trackUrl })
           height="300"
           scrolling="no"
           frameBorder="no"
-          /* 
-            - Recomendamos incluir explicitamente allow="autoplay" (e nada mais difícil de ser interpretado pelo navegador móvel),
-              para evitar que o player seja bloqueado por políticas de segurança mobile.
-          */
           allow="autoplay"
-          src={`https://w.soundcloud.com/player/?url=${encodeURIComponent(trackUrl)}&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true`}
+          src={`https://w.soundcloud.com/player/?
+            url=${encodeURIComponent(trackUrl)}
+            &color=%23ff5500
+            &auto_play=false
+            &hide_related=false
+            &show_comments=true
+            &show_user=false
+            &show_reposts=false
+            &show_teaser=true
+            &visual=true`
+              .replace(/\s+/g, '')
+          }
         ></iframe>
       )}
     </div>
