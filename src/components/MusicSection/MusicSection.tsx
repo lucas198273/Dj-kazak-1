586238a41a3a8c77c77a1021d6d07b155ec00b72
Lucas Dias <lucasinga2000@gmail,com>
2025-06-01 12:05:17 -0300

    1

--- src/components/MusicSection/MusicSection.tsx
+++ src/components/MusicSection/MusicSection.tsx
@@ -19,8 +19,8 @@ const MusicSection: React.FC = () => {
               allow="autoplay"
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1904481743&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
             ></iframe>
-            {/* Camada de bloqueio para impedir cliques diretos */}
-            <div className="absolute top-0 left-0 w-full h-full z-10 pointer-events-none"></div>
+            {/* Camada de bloqueio para impedir cliques (pointer-events-auto) */}
+            <div className="absolute top-0 left-0 w-full h-full z-10 bg-transparent pointer-events-auto" />
           </div>
 
           {/* Player 2 */}
@@ -33,7 +33,7 @@ const MusicSection: React.FC = () => {
               allow="autoplay"
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1897798665&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
             ></iframe>
-            <div className="absolute top-0 left-0 w-full h-full z-10 pointer-events-none"></div>
+            <div className="absolute top-0 left-0 w-full h-full z-10 bg-transparent pointer-events-auto" />
           </div>
 
           {/* Player 3 */}
@@ -46,7 +46,7 @@ const MusicSection: React.FC = () => {
               allow="autoplay"
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1825949580&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
             ></iframe>
-            <div className="absolute top-0 left-0 w-full h-full z-10 pointer-events-none"></div>
+            <div className="absolute top-0 left-0 w-full h-full z-10 bg-transparent pointer-events-auto" />
           </div>
 
           {/* Player 4 */}
@@ -59,7 +59,7 @@ const MusicSection: React.FC = () => {
               allow="autoplay"
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1823157531&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
             ></iframe>
-            <div className="absolute top-0 left-0 w-full h-full z-10 pointer-events-none"></div>
+            <div className="absolute top-0 left-0 w-full h-full z-10 bg-transparent pointer-events-auto" />
           </div>
 
           {/* Player 5 */}
@@ -72,7 +72,7 @@ const MusicSection: React.FC = () => {
               allow="autoplay"
               src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1950042995&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
             ></iframe>
-            <div className="absolute top-0 left-0 w-full h-full z-10 pointer-events-none"></div>
+            <div className="absolute top-0 left-0 w-full h-full z-10 bg-transparent pointer-events-auto" />
           </div>
         </div>
 
