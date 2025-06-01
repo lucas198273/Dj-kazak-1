67aafecaa51c35aefa2d4a719a61b836a64f21d7
Lucas Dias <lucasinga2000@gmail,com>
2025-06-01 14:03:02 -0300

    0

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,8 +7,9 @@ export default function About() {
   return (
     <div className="about-page">
       <AboutIntro />
-      <AboutGallery />
+      
       <SocialMediaSection />
+      <AboutGallery />
     </div>
   );
 }
