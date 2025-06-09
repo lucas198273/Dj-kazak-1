d36297eff6017103ec3bba061573d7074c395ef6
Lucas Dias <lucasinga2000@gmail,com>
2025-06-09 19:45:03 -0300

    1

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,12 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
-
   return (
-    <header
-     
-      className="fixed inset-x-0 top-0 h-20 bg-gradient-to-r bg-white text-black z-50 shadow-lg"
-    >
+    <header className="fixed inset-x-0 top-0 h-20 bg-white text-black z-50 shadow-lg">
       <div className="max-w-7xl mx-auto h-full px-6 flex items-center justify-between relative">
         {/* Botão Mobile */}
         <button
@@ -23,31 +18,26 @@ export default function Header() {
           {menuOpen ? '✕' : '☰'}
         </button>
 
-        {/* Menu Desktop */}
-        <nav
-          className="hidden md:flex space-x-8 font-medium text-black"
-       
-        >
-          <Link to="/" className="hover:text-blue-700 transition">Início</Link>
-          <Link to="/about" className="hover:text-blue-700 transition">Sobre</Link>
-        </nav>
-
         {/* Logo Centralizada */}
-        <div
-          className="absolute left-1/2 transform -translate-x-1/2 mt-6"
-         
-        >
+        <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 pointer-events-none md:pointer-events-auto">
           <Link to="/">
             <img
               src="/assets/logo3.png"
               alt="Logo"
-              className="h-30 w-auto object-contain"
+              className="h-25 w-auto object-contain"
+              loading="lazy"
             />
           </Link>
         </div>
+
+        {/* Menu Desktop */}
+        <nav className="hidden md:flex space-x-8 font-medium text-black">
+          <Link to="/" className="hover:text-blue-700 transition">Início</Link>
+          <Link to="/about" className="hover:text-blue-700 transition">Sobre</Link>
+        </nav>
       </div>
 
-      {/* Menu Mobile: aparece só quando menuOpen for true */}
+      {/* Menu Mobile */}
       {menuOpen && (
         <nav
           className="fixed top-20 left-0 w-64 h-[calc(100vh-5rem)] bg-white text-black shadow-lg md:hidden z-40 p-6 flex flex-col space-y-6"
